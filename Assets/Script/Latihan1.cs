d9515989218130be4822f1c8d40f1465b5f4477d
Azmi Munif Hartono <azmimunifhartono@gmail.com>
2025-09-06 20:07:38 +0700

    Menambahkan Asser Dasar Game

---
+++ Assets/Script/Latihan1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Belajar C# Pertama Di Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Belajar C# Pertama Di Unity! Lagi!");
+    }
+}
